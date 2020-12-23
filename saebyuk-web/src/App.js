e3fb70329ea72f60fa104498f77eab0b9206c147
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-12-24 02:36:51 +0900

    Kakao Login Completed

--- saebyuk-web/src/App.js
+++ saebyuk-web/src/App.js
@@ -4,13 +4,44 @@ import React from 'react';
 import Cookies from 'js-cookie';
 
 function App() {
+  const [profile, setProfile] = React.useState({});
+  const [gSchoolNickname, setNickname] = React.useState('');
   
   React.useEffect(( ) => {
     window.Kakao.init('ef9a576a8e9f7b59585eb0e36c820fad');
     Kakao.isInitialized();
   }, []);
+
+  const signUp = () => {
+    console.log(profile);
+    const kakao_profile_info = profile.kakao_profile;
+    console.log(kakao_profile_info);
+    const csrftoken = Cookies.get('csrftoken');
+    axios.post('http://127.0.0.1:8000/account/sign-up/kakao/', {
+      headers:{
+          "Access-Control-Allow-Origin": '*',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken
+      },
+      data: {
+        kakao_id: profile.kakao_id,
+        g_school_nickname: gSchoolNickname,
+        kakao_nickname: kakao_profile_info.nickname,
+        profile_image: kakao_profile_info.thumbnail_image_url,
+        access_token: profile.access_token
+      }
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err))
+
+
+
+  }
   
-  const login = () => {
+  const signIn = () => {
     axios.defaults.xsrfCookieName = "csrftoken"
     axios.defaults.xsrfHeaderName = "X-CSRFToken"
     Kakao.Auth.login({
@@ -31,7 +62,14 @@ function App() {
             access_token: res.access_token
           }
         })
-        .then((res) => console.log(res))
+        .then((res) => {
+          // const kakaoProfile = JSON.parse(JSON.stringify(res));
+          if (res.status === 203) {
+            setProfile(res.data);
+          } else if (res.status === 200) {
+            console.log(res.data);
+          }
+        })
         .catch((err) => console.log(err))
       }, 
       fail: (err) => {
@@ -43,7 +81,9 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={login}>로그인</button>
+      <button onClick={signIn}>로그인</button>
+      <input type="text" value={gSchoolNickname} onChange={(e) => setNickname(e.target.value)} maxLength={2} />
+      <button onClick={signUp}>닉네임 등록</button>
     </div>
   );
 }
